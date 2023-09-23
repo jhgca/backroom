ac7f13c06389500cb1186e48ed48ebc175d00840
jhgca <121985367+jhgca@users.noreply.github.com>
2023-09-23 11:43:48 -0700

    Initial commit

---
+++ Assets/move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Adjust this to control the movement speed.
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Get input from the arrow keys or WASD keys.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction.
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+
+        // Apply the movement to the Rigidbody2D.
+        rb.velocity = movement * moveSpeed;
+    }
+}
