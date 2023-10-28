dfbb13fe3acf596c011995b7058c14357eb2945f
jhgca <121985367+jhgca@users.noreply.github.com>
2023-10-28 12:01:52 -0700

    timer + cane indicator + gofast
    raiden go fast slo

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager main;
     public int canes;
+    public TextMeshProUGUI canetxt;
+    public TextMeshProUGUI timer;
     public void changecanes(int amount)
     {
         canes += amount;
+        canetxt.text = "kains saus: " + canes;
+    }
+    public void countdown(int time)
+    {
+        timer.text = "angerys: " + time + "/120";
     }
     // Start is called before the first frame update
     void Start()
--- Assets/chase.cs
+++ Assets/chase.cs
@@ -3,54 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 using UnityEditor.Experimental.GraphView;
+using System;
 
 public class chase : MonoBehaviour
 {
     public GameObject target;
     private Rigidbody2D rb;
-    public float speed;
+    public float minspeed, maxspeed;
+    public float timetomax;
     private Seeker seeker;
-    private Path path;
-    public int currentwaypoint = 0;
-    private bool reachedendofpath = false;
-    public Vector2 direction;
-    public Vector3 currtarget;
+    private float startTime;
+    //private Path path;
+    //public int currentwaypoint = 0;
+    //private bool reachedendofpath = false;
+    //public Vector2 direction;
+    //public Vector3 currtarget;
+    public IAstarAI ai;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         seeker = GetComponent<Seeker>();
+        ai = GetComponent<IAstarAI>();
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float currtime = (Time.time - startTime);
+        float currspeed = Mathf.Lerp(minspeed, maxspeed, (currtime)/timetomax);
+        GameManager.main.countdown((int)currtime);
+        ai.maxSpeed = currspeed;
         Vector3 destination = target.transform.position;
-        seeker.StartPath(transform.position, destination, OnPathComplete);
-        if (path != null ) 
-        {
-            if (currentwaypoint >= path.vectorPath.Count) 
-            {
-                reachedendofpath = true;
-            }
-            else
-            {
-                reachedendofpath = false;
-                currtarget = path.vectorPath[currentwaypoint] - transform.position;
-                direction = currtarget.normalized;
+        ai.destination = destination;
+        //seeker.StartPath(transform.position, destination, OnPathComplete);
+        //if (path != null ) 
+        //{
+        //    if (currentwaypoint >= path.vectorPath.Count) 
+        //    {
+        //        reachedendofpath = true;
+        //    }
+        //    else
+        //    {
+        //        reachedendofpath = false;
+        //        currtarget = path.vectorPath[currentwaypoint] - transform.position;
+        //        direction = currtarget.normalized;
 
-            }
-        }
+        //    }
+        //}
         
-        rb.velocity = direction * speed;
+        //rb.velocity = direction * speed;
     }
-    void OnPathComplete(Path p)
-    {
-        if (!p.error)
-        {
-            path = p;
-            currentwaypoint = 1;
-            reachedendofpath = false;
-        }
-    }
+    //void OnPathComplete(Path p)
+    //{
+    //    if (!p.error)
+    //    {
+    //        path = p;
+    //        currentwaypoint = 1;
+    //        reachedendofpath = false;
+    //    }
+    //}
 }
